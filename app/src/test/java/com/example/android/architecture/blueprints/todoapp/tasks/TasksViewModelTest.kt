d7434c8346f3ad40fdd7cd2cf3c7c073f113daa8
andersgaustad <anders.gaustad@gmail.com>
2020-08-01 10:31:39 +0200

    Set up testing for LiveData

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,19 +1,33 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import getOrAwaitValue
+import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.nullValue
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext()).apply {
             addNewTask()
+
+            val value = newTaskEvent.getOrAwaitValue()
+
+            assertThat(value.getContentIfNotHandled(), not(nullValue()))
         }
+
+
     }
 
 
